fdcff702579d5dab905c84e405ba7fa79f709e3b
Nicfrey <nicolas.fer9@gmail.com>
2024-12-04 20:32:08 +0100

    feat: click interaction

--- Assets/Scripts/Player/MouseInteraction.cs
+++ Assets/Scripts/Player/MouseInteraction.cs
@@ -1,5 +1,6 @@
 using Input;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 using UnityEngine.InputSystem;
 
 namespace Player
@@ -7,17 +8,24 @@ namespace Player
     public class MouseInteraction : MonoBehaviour
     {
         [SerializeField] private InteractionReader _reader;
-        private Vector2 _screenPosition; 
+        private Vector2 _screenPosition;
+        private Camera _camera;
 
         private void Awake()
         {
+            _camera = Camera.main;
             _reader.OnMoveEvent.AddListener(GetScreenPosition);
             _reader.OnClickEvent.AddListener(Click);
         }
 
         private void Click()
         {
-            
+            Ray ray = _camera.ScreenPointToRay(_screenPosition);
+            if (Physics.Raycast(ray, out var hit, Mathf.Infinity,LayerMask.GetMask("Ground")))
+            {
+                Debug.Log(hit.point);
+                Instantiate(GameObject.CreatePrimitive(PrimitiveType.Sphere), hit.point, Quaternion.identity);
+            }
         }
 
         private void GetScreenPosition(Vector2 screenPosition)
